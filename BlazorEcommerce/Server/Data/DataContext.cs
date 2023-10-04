2d5c60bb12e22eb7f6932608d14628008453bcc6
jmaetogonon <jochellemae16@gmail.com>
2023-10-04 20:05:36 +0800

    Add project files.

---
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -0,0 +1,33 @@
+﻿namespace BlazorEcommerce.Server.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Product>().HasData(
+				new Product
+				{
+					Id = 1,
+					Title = "The Hitchhiker's Guide to the Galaxy",
+					Description = "The Hitchhiker's Guide to the Galaxy[note 1] (sometimes referred to as HG2G,[1] HHGTTG,[2] H2G2,[3] or tHGttG) is a comedy science fiction franchise created by Douglas Adams. Originally a 1978 radio comedy broadcast on BBC Radio 4, it was later adapted to other formats, including stage shows, novels, comic books, a 1981 TV series, a 1984 text-based computer game, and 2005 feature film.",
+					ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+					Price = 9.99m
+				},
+				new Product
+				{
+					Id = 2,
+					Title = "Ready Player One",
+					Description = "Ready Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline. The story, set in a dystopia in 2045, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game, the discovery of which would lead him to inherit the game creator's fortune. Cline sold the rights to publish the novel in June 2010, in a bidding war to the Crown Publishing Group (a division of Random House).[1] The book was published on August 16, 2011.[2] An audiobook was released the same day; it was narrated by Wil Wheaton, who was mentioned briefly in one of the chapters.[3][4]Ch. 20 In 2012, the book received an Alex Award from the Young Adult Library Services Association division of the American Library Association[5] and won the 2011 Prometheus Award.[6]",
+					ImageUrl = "https://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg",
+					Price = 7.99m
+				}
+			);
+		}
+
+		public DbSet<Product> Products { get; set; }
+
+	}
+
+}
